4966c8d59c4644ad5dad397149e735652d1c4314
seunggyu <seunggyujung008@gmail.com>
2023-10-20 17:45:05 +0900

    :memo:docs: README 오타 수정 및 styled-components 설치 :sparkles:feat: GlobalStyle 스타일링 컴포넌트 추가

---
+++ src/components/style/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default GlobalStyle = createGlobalStyle`
+ :root{
+    --main-color: #036635 ;
+    --sub-color:#B1D4C3;
+    --font-color: #000000;
+    --sub-font-color: #767676;;
+    --border-color: #DBDBDB ;
+    --background-color: #F2F2F2;
+    --error-color: #EB5757;
+    --font-xl : 20px;
+    --font-lg : 16px;
+    --font-md : 14px;
+    --font-sm : 12px;
+  }
+
+    :root {
+    --font--bold: 700;
+    --font--semibold: 500;
+    --font-regular: 400;;
+  }
+`;
