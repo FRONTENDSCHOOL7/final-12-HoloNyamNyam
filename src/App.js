056dd109b7ed614f510ef9d9e24a61c26a8cf736
seunggyu <seunggyujung008@gmail.com>
2023-10-20 23:03:53 +0900

    :memo:docs: eslint-config-plugin-prettier 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import './App.css';
+import Signup from './pages/AuthorPage/Signup';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Signup />
+    </div>
+  );
 }
 
 export default App;
