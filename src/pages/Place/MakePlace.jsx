eec5d41aeb5831121b9ded1a1539f3ad4a592e9e
seunggyu <seunggyujung008@gmail.com>
2023-10-22 14:03:26 +0900

    :sparkle:feat: routers 초기 설정

---
+++ src/pages/Place/MakePlace.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MakePlace() {
+  return <div>MakePlace</div>;
+}
---
+++ src/routers/Routers.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Splash from '../pages/Splash/Splash';
+import Welcome from '../pages/Welcome/Welcome';
+import Login from '../pages/Login/Login';
+import SignUp from '../pages/SignUp/SignUp';
+import SignUpSuccess from '../pages/SignUpSuccess';
+import ProfileSetting from '../pages/Profile/ProfileSetting';
+import Home from '../pages/Home/Home';
+import Search from '../pages/Search/Search';
+import ChatList from '../pages/Chat/ChatList';
+import ErrorPage from '../pages/Error/ErrorPage';
+import ChatRoom from '../pages/Chat/ChatRoom';
+import MakeFeed from '../pages/Feed/MakeFeed';
+import Profile from '../pages/Profile/Profile';
+import ProfileEdit from '../pages/Profile/ProfileEdit';
+import DetailFeed from '../pages/Feed/DetailFeed';
+import FollowerList from '../pages/FollowerList/FollowerList';
+import PlaceList from '../pages/Place/PlaceList';
+import MakePlace from '../pages/Place/MakePlace';
+import Map from '../pages/Place/Map';
+
+export default function Routers() {
+  return (
+    <Routes>
+      <Route path='/' element={<Splash />} />
+      <Route path='/welcome' element={<Welcome />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup'>
+        <Route index element={<SignUp />} />
+        <Route path='profile' element={<ProfileSetting />} />
+        <Route path='signupsuccess' element={<SignUpSuccess />} />
+      </Route>
+      <Route path='/home' element={<Home />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/chat' element={<ChatList />} />
+      <Route path='/chatroom/:accountname' element={<ChatRoom />} />
+      <Route path='/error' element={<ErrorPage />} />
+      <Route path='/makefeed' element={<MakeFeed />} />
+      <Route path='/detailfeed' element={<DetailFeed />} />
+      <Route path='/myprofile'>
+        <Route index element={<Profile type='my' />} />
+        <Route path='edit' element={<ProfileEdit />} />
+      </Route>
+      <Route path='/profile/:accountname'>
+        <Route index element={<Profile type='your' />} />
+      </Route>
+      <Route
+        path='/followerlist'
+        element={<FollowerList type='followers' followType='followerList' />}
+      />
+      <Route
+        path='/followinglist'
+        element={<FollowerList type='followings' followType='followingList' />}
+      />
+      <Route path='/placelist' element={<PlaceList />} />
+      <Route path='/makeplace' element={<MakePlace />} />
+      <Route path='/map' element={<Map />} />
+    </Routes>
+  );
+}
