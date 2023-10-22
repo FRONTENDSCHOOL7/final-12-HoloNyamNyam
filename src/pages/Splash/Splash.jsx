03035d2c01fc35f19861b85b32982bd670f1e2e7
sumin.oh <osm960110@gmail.com>
2023-10-22 23:56:13 +0900

    ✨feat:  splash 화면 구현

--- src/pages/Splash/Splash.jsx
+++ src/pages/Splash/Splash.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import LogoBowlImg from '../../images/logo_bowl.svg';
+import LogoNameImg from '../../images/logo_name.svg';
+import { Appear, LogoBowl, LogoName } from './SplashStyle';
 
 export default function Splash() {
-  return <div>Splash</div>;
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const [fadeOut, setFadeOut] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      setTimeout(() => {
+        setFadeOut(true);
+        setTimeout(() => {
+          navigate('/home');
+        }, 500);
+      }, 3000);
+    } else {
+      const timer = setTimeout(() => {
+        setFadeOut(true);
+        setTimeout(() => {
+          navigate('/welcome');
+        }, 500);
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [navigate, token]);
+
+  return (
+    <Appear fadeOut={fadeOut}>
+      <LogoBowl src={LogoBowlImg} alt='' className='logo bowl' />
+      <LogoName src={LogoNameImg} alt='' className='logo name' />
+    </Appear>
+  );
 }
--- src/pages/Splash/SplashStyle.jsx
+++ src/pages/Splash/SplashStyle.jsx
@@ -1,5 +1,53 @@
-import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
-export default function SplashStyle() {
-  return <div>SplashStyle</div>;
+const AppearAnimation = keyframes`
+0% {
+  opacity: 0;
 }
+100% {
+  opacity: 1;
+}
+`;
+
+const Appear = styled.div`
+  animation: ${AppearAnimation} 1s ease;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const BowlAnimation = keyframes`
+0%, 20%, 50%, 80%, 100% {transform: translateY(0);}
+  40% {transform: translateY(-30px);}
+  60% {transform: translateY(-15px);}
+`;
+
+const LogoBowl = styled.img`
+  animation: ${BowlAnimation} 2s ease;
+
+  display: block;
+  filter: drop-shadow(3px 3px 3px rgba(0, 0, 0, 0.25));
+`;
+
+const NameAnimation = keyframes`
+0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+`;
+
+const LogoName = styled.img`
+  animation: ${NameAnimation} 2s ease;
+
+  display: block;
+  filter: drop-shadow(3px 3px 3px rgba(0, 0, 0, 0.25));
+`;
+
+export { Appear, LogoBowl, LogoName };
