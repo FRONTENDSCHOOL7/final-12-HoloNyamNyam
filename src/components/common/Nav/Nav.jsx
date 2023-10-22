b824d590ef1f7c681805a19564f320e8c617c9c8
gee0916 <hgee9816@gmail.com>
2023-10-22 20:34:03 +0900

    :sparkles:feat: nav 라우터 연결

--- src/components/common/Nav/Nav.jsx
+++ src/components/common/Nav/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
   NavWrapper,
   NavList,
@@ -13,9 +14,10 @@ import topIcon from '../../../images/arrow_top.svg';
 
 export default function Nav() {
   const [showButton, setShowButton] = useState(false);
+  const location = useLocation();
   const NavSVG = ({ id, color = 'white', size = 24 }) => (
     <svg fill={color} width={size} height={size}>
-      <use href={`${sprite}#${id}`} style={{ stroke: 'currentColor' }} />
+      <use href={`${sprite}#${id}`} />
     </svg>
   );
 
@@ -51,24 +53,73 @@ export default function Nav() {
       </ButtonContainer>
       <NavList>
         <li>
-          <NavSVG id='icon-home-fill' />
-          <StyledNavText>홈</StyledNavText>
+          <NavLink
+            to='/home'
+            className={`nav-link ${
+              location.pathname === '/home' ? 'active' : ''
+            }`}
+          >
+            <NavSVG
+              id={
+                location.pathname === '/home' ? 'icon-home-fill' : 'icon-home'
+              }
+            />
+            <StyledNavText>홈</StyledNavText>
+          </NavLink>
         </li>
         <li>
-          <NavSVG id='icon-search' />
-          <StyledNavText>검색</StyledNavText>
+          <NavLink
+            to='/search'
+            className={`nav-link ${
+              location.pathname === '/search' ? 'active' : ''
+            }`}
+          >
+            <NavSVG
+              id={
+                location.pathname === '/search'
+                  ? 'icon-search-fill'
+                  : 'icon-search'
+              }
+            />
+            <StyledNavText>검색</StyledNavText>
+          </NavLink>
         </li>
         <li>
-          <NavSVG id='icon-edit' />
-          <StyledNavText>게시물 작성</StyledNavText>
+          <NavLink to='/makefeed'>
+            <NavSVG id='icon-edit' />
+            <StyledNavText>게시물 작성</StyledNavText>
+          </NavLink>
         </li>
         <li>
-          <NavSVG id='icon-message-circle' />
-          <StyledNavText>채팅</StyledNavText>
+          <NavLink
+            to='/chat'
+            className={`nav-link ${
+              location.pathname === '/chat' ? 'active' : ''
+            }`}
+          >
+            <NavSVG
+              id='icon-message-circle'
+              color={location.pathname === '/chat' ? '#f26e22' : 'white'}
+            />
+            <StyledNavText>채팅</StyledNavText>
+          </NavLink>
         </li>
         <li>
-          <NavSVG id='icon-user-fill' />
-          <StyledNavText>프로필</StyledNavText>
+          <NavLink
+            to='/myprofile'
+            className={`nav-link ${
+              location.pathname === '/myprofile' ? 'active' : ''
+            }`}
+          >
+            <NavSVG
+              id={
+                location.pathname === '/myprofile'
+                  ? 'icon-user-fill'
+                  : 'icon-user'
+              }
+            />
+            <StyledNavText>프로필</StyledNavText>
+          </NavLink>
         </li>
       </NavList>
     </NavWrapper>
