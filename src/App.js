1490b6d00ee28800feb3e63ef09b34680c9b1da3
redcontroller <jasinis102@gmail.com>
2023-10-24 10:42:42 +0900

    :sparkles:feat: 피드 없는 Home 페이지 구현 및 기본 검색 페이지 레이아웃 #18

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { GlobalStyle } from './components/style/GlobalStyle';
 import Routers from './routers/Routers';
-import Nav from './components/common/Nav/Nav';
 
 function App() {
   return (
