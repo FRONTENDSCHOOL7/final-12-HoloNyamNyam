8000d79841eb0eb7f34a06ac049ea3563d7de406
sumin.oh <osm960110@gmail.com>
2023-10-24 21:22:05 +0900

    :sparkles:feat: chat 컴포넌트와 chat list 구현

--- src/pages/Chat/ChatList.jsx
+++ src/pages/Chat/ChatList.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import Nav from '../../components/common/Nav/Nav';
+import ChatListItem from '../../components/Chat/ChatListItem';
+import Header from '../../components/common/Header/Header';
 
+const List = styled.section`
+  padding: 48px 0 60px 0;
+  background-color: white;
+  height: calc(100vh - 108px);
+`;
 export default function ChatList() {
-  return <div>ChatList</div>;
+  return (
+    <>
+      <Header type='default' />
+      <List>
+        <ChatListItem />
+      </List>
+      <Nav />
+    </>
+  );
 }
