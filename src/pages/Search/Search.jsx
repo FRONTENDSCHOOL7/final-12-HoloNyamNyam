19fbe1cf42a2465c853de476878904070a45492d
sumin.oh <osm960110@gmail.com>
2023-10-26 15:53:15 +0900

    ✨feat:  search 기능 구현

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Nav from '../../components/common/Nav/Nav';
+import SearchList from '../../components/Search/SearchList';
 import Header from '../../components/common/Header/Header';
+
+const List = styled.section`
+  padding: 48px 0 60px 0;
+  height: calc(100vh - 108px);
+  background-color: white;
+`;
 
 export default function Search() {
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -14,7 +22,10 @@ export default function Search() {
         searchKeyword={searchKeyword}
         handleSearchKeyword={handleSearchKeyword}
       />
-      <div>Search</div>
+      <List>
+        <SearchList searchKeyword={searchKeyword} />
+      </List>
+      <div>search</div>
       <Nav />
     </>
   );
