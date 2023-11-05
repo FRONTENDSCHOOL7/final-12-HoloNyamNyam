4e8292aacc1113cfdbd51ef3b96926acdeb9dd31
gee0916 <hgee9816@gmail.com>
2023-11-06 00:33:44 +0900

    :bug:fix: 회원가입 과정 return 해결

--- src/pages/ProfileSetting/ProfileSetting.jsx
+++ src/pages/ProfileSetting/ProfileSetting.jsx
@@ -1,26 +1,6 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import ProfileSettingForm from '../../components/ProfileSetting/ProfileSettingForm';
 
 export default function ProfileSetting() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (
-      !sessionStorage.getItem('_id') ||
-      !sessionStorage.getItem('accountname') ||
-      !sessionStorage.getItem('token')
-    ) {
-      navigate('/');
-    }
-  }, [navigate]);
-
-  if (
-    !sessionStorage.getItem('_id') ||
-    !sessionStorage.getItem('accountname') ||
-    !sessionStorage.getItem('token')
-  ) {
-    return null;
-  }
   return <ProfileSettingForm />;
 }
